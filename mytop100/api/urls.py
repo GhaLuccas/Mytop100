c273e8949d65ab26951898272cba2cb38280cc9c
ghaluccas <gabsgit@gmail.com>
2024-10-23 13:54:05 -0300

    files update

--- mytop100/api/urls.py
+++ mytop100/api/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path("movies/", views.get_all_movies, name="get_all"),
-    path("movie/<int:movie_id>/", views.get_one_movie, name="get_one"),  
-    path("remove/<int:movie_id>/" , views.delete_movie, name="delete_movie") ,
+    path("movie/<int:movie_id>/", views.get_one_movie, name="get_one"),
+    path("create-movie/" , views.create_movie , name="ceate-movie"),
+    path("remove-movie/<int:movie_id>/" , views.delete_movie, name="delete_movie") ,
     path("generic/movies/", views.GenericListAllAPI.as_view(), name="generic_all"),  
     path("generic/movie/<int:movie_id>/", views.GenericMovieRetrieveAPI.as_view(), name="generic_one"),
     path("generic/delete/<int:movie_id>/", views.GenericDeleteMovieAPI.as_view() , name="generic_del"),
