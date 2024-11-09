198828392c1ed288fde7a1027e373205b94ab4fb
GhaLuccas <gitsgab@gmail.com>
2024-11-09 14:50:49 -0300

    home page added to login/register html

--- mytop100/movies/views.py
+++ mytop100/movies/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render , redirect
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from .models import Movie, MovieList
 
@@ -8,6 +9,7 @@ from rest_framework.response import Response
 
 
 from .serializers import MovieSerializer
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
@@ -57,3 +59,19 @@ def remove_movie(request, movie_id):
     except ValueError as e:
         print(e)
     return redirect('list') 
+
+
+@login_required
+@csrf_exempt
+def add_movie(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)  # Lê os dados JSON enviados na requisição
+            movie_id = data.get('movie_id')
+            movie = get_object_or_404(Movie, id=movie_id)
+            movie_list, created = MovieList.objects.get_or_create(user=request.user)
+            movie_list.add_movie(movie)
+            return JsonResponse({'message': 'Filme adicionado com sucesso!'}, status=201)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
+    return JsonResponse({'error': 'Método não permitido'}, status=405)
